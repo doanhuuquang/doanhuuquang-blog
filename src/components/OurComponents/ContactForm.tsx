b3b40c56f41bbccf3ad0778225362c07e1d1f9a3
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-05-13 10:18:06 +0700

    Redesign input style

--- src/components/OurComponents/ContactForm.tsx
+++ src/components/OurComponents/ContactForm.tsx
@@ -32,6 +32,8 @@ export default function ContactForm() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
+      email: "",
+      phone: "",
     },
   });
 
@@ -65,7 +67,7 @@ export default function ContactForm() {
         </div>
         {/* Social link */}
         <div className="flex flex-wrap gap-5">
-          <Button className="bg-[#0866FF] hover:bg-[#0866FF]">
+          <Button className="bg-[#0866FF] hover:bg-[#0866FF] w-[40px] h-[40px] rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 320 512"
@@ -74,7 +76,7 @@ export default function ContactForm() {
               <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
             </svg>
           </Button>
-          <Button className="bg-[#7289da] hover:bg-[#7289da]">
+          <Button className="bg-[#7289da] hover:bg-[#7289da] w-[40px] h-[40px] rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 640 512"
@@ -83,7 +85,7 @@ export default function ContactForm() {
               <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
             </svg>
           </Button>
-          <Button className="bg-[#FF0000] hover:bg-[#FF0000]">
+          <Button className="bg-[#FF0033] hover:bg-[#FF0033] w-[40px] h-[40px] rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 576 512"
