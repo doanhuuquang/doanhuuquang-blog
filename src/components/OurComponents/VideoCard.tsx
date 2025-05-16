a0dbe5daf3f63a56b408ea22ce757e8d4f9f7fa9
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-05-16 11:02:45 +0700

    Save change

--- src/components/OurComponents/VideoCard.tsx
+++ src/components/OurComponents/VideoCard.tsx
@@ -12,22 +12,22 @@ type Video = {
 
 export default function VideoCard({ video }: { video: Video }) {
   return (
-    <Card className="lg:col-span-1 gap-0 md:col-span-2 col-span-4 group overflow-hidden p-0 hover:cursor-pointer">
+    <Card className="flex flex-row lg:flex-col lg:col-span-1 gap-0 md:col-span-2 col-span-4 group overflow-hidden p-0 hover:cursor-pointer">
       <Image
-        className="w-full"
+        className="lg:w-full w-[50%]"
         src={video.image}
         width={300}
         height={100}
         alt={video.title}
       />
-      <CardContent className="p-4 h-full flex flex-col justify-between">
-        <h4 className="text-lg line-clamp-2">
+      <CardContent className="lg:w-full h-full w-[50%] flex lg:flex-col flex-col  p-4 items-start justify-between">
+        <h4 className="text-lg w-full line-clamp-2">
           <Link href={video.href}>
             <span aria-hidden="true" className="absolute inset-0" />
             {video.title}
           </Link>
         </h4>
-        <div className="flex justify-between">
+        <div className="flex justify-between w-full lg:flex-row flex-col">
           <p className="text-sm text-muted-foreground">
             {video.views} Lượt xem
           </p>
